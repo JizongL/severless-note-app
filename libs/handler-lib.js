8a38d85c17192e1a1490aa711527ac5d53098fbf
Jizong Liang <Liang_Jizong@comcast.com>
2020-09-26 00:05:46 -0400

    Adding error logging

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,12 +1,16 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
-
+    // Start debugger
+    debug.init(event, context);
     try {
       // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
+      debug.flush(e);
       body = { error: e.message };
       statusCode = 500;
     }
